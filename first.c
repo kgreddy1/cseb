ff1db18854c35539a8e9c191c141de184cd36120
gopircee <emailme.kallam@rcee.ac.in>
2023-08-21 11:00:38 +0530

    C and Python files are committed

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    printf("\nEnter two integers  : ");
+    scanf("%d%d",&a,&b);
+    c = a + b;
+    printf("Addition of Two Integers : %d",c);
+    return 0;
+}
